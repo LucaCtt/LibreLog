8d99bd7c7198b0552815eac2bcc2ec0b527f79f8
Luca Cotti <lucacotti@outlook.com>
2025-02-10 17:06:20 +0100

    feat: Remove unneeded stuff

--- models/README.md
+++
@@ -1,3 +0,0 @@
-## Models download
-
-Please download the base LLM (Meta-Llama-3-8B-Instruct) from [Huggingface](https://huggingface.co/meta-llama/Meta-Llama-3-8B-Instruct).
--- parsing.sh
+++
@@ -1,3 +0,0 @@
-cd parser
-
-python evaluator.py --project "Apache,HPC,OpenSSH,Zookeeper,OpenStack,Linux,Proxifier,HealthApp,HDFS,Hadoop,Spark,BGL,Mac,Thunderbird"  --sample "3"
